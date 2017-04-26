fe08d97b705e653a3f57081146ecdcf1b510f4da
JunjieW <junjie.wei@dreamcorps.org>
2017-04-26 18:14:40 -0400

    init CUDA template code for Mandelbrot set

---
+++ mandelbrot_gpu.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+'''
+Advanced Python assignemtn 12:
+    mandelbrot.py
+'''
+
+
+# 
+# A CUDA version to calculate the Mandelbrot set
+#
+from numba import cuda
+import numpy as np
+from pylab import imshow, show
+
+@cuda.jit(device=True)
+def mandel(x, y, max_iters):
+    '''
+    Given the real and imaginary parts of a complex number,
+    determine if it is a candidate for membership in the 
+    Mandelbrot set given a fixed number of iterations.
+    '''
+    c = complex(x, y)
+    z = 0.0j
+    for i in range(max_iters):
+        z = z*z + c
+        if (z.real*z.real + z.imag*z.imag) >= 4:
+            return i
+
+    return max_iters
+
+@cuda.jit
+def compute_mandel(min_x, max_x, min_y, max_y, image, iters):
+    '''
+    YOUR COMMENT HERE
+    '''
+    ### YOUR CODE HERE
+    
+if __name__ == '__main__':
+    image = np.zeros((1024, 1536), dtype = np.uint8)
+    blockdim = (32, 8)
+    griddim = (32, 16)
+    
+    image_global_mem = cuda.to_device(image)
+    compute_mandel[griddim, blockdim](-2.0, 1.0, -1.0, 1.0, image_global_mem, 20) 
+    image_global_mem.copy_to_host()
+    imshow(image)
+    show()
